77c7762911a6bd25da743c599dc34e321ddcc373
𝐁𝐚𝐘𝐣𝐢𝐝 <aryanboss2@yahoo.com>
2025-07-07 22:15:56 +0600

    Update say.js

--- scripts/cmds/say.js
+++ scripts/cmds/say.js
@@ -1,20 +1,29 @@
-module.exports = {
-	config: {
-		name: "say2",
-		version: "1.0",
-		author: "Samir", // Time to wait before executing command again (seconds)
-		role: 0,
-		category: "text",
-		guide: {
-			vi: "Not Available",
-			en: "says + (Message You Want To Get)"
-		} 
-	},
-
-	onStart: async function ({ api, args, event }) {
-	var say = args.join(" ")
-	if (!say) api.sendMessage("Please enter a message", event.threadID, event.messageID)
-	else api.sendMessage(`${say}`, event.threadID, event.messageID);
+module.exports.config = {
+	name: "say",
+	version: "1.6.9",
+	role: 0,
+	author: "BaYjid",
+	description: "Get Bangla Voice",
+	category: "media",
+	guide: {
+   en: " {pn} text"
+   },
+	countDowns: 5,
+	dependencies: {
+		"path": "",
+		"fs-extra": ""
 	}
-
 };
+
+module.exports.onStart = async function({ api, event, args }) {
+	try {
+		const { createReadStream, unlinkSync } = require('fs-extra');
+		const { resolve } = require('path');
+		var content = (event.type == "message_reply") ? event.messageReply.body : args.join(" ");
+		var languageToSay = (["bn",].some(item => content.indexOf(item) == 0)) ? content.slice(0, content.indexOf(" ")) : global.GoatBot.config.language;
+		var msg = (languageToSay != global.GoatBot.config.language) ? content.slice(3, content.length) : content;
+		const path = resolve(__dirname, 'cache', `${event.threadID}_${event.senderID}.mp3`);
+		await global.utils.downloadFile(`https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(msg)}&tl=bn&client=tw-ob`, path);
+		return api.sendMessage({ attachment: createReadStream(path)}, event.threadID, () => unlinkSync(path));
+	} catch (e) { return console.log(e) };
+				       }
