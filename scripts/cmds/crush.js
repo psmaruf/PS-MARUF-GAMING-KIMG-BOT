fde7d36e435e48a57e2e08e0d141ec77020caea0
BAYJID-403 <merrybawm998@gmail.com>
2025-07-19 18:06:25 +0600

    Update crush.js

--- scripts/cmds/crush.js
+++ scripts/cmds/crush.js
@@ -1,14 +1,14 @@
 const fs = require("fs-extra");
 const axios = require("axios");
 const path = require("path");
-const jimp = require("jimp");
+const jimp = global.nodemodule["jimp"];
 
 module.exports = {
   config: {
     name: "crush",
     aliases: [],
     version: "1.0",
-    author: "Priyansh Rajput + Modified by RAHAD",
+    author: "Priyansh Rajput + Fixed by RAHAD",
     countDown: 5,
     role: 0,
     shortDescription: {
@@ -23,7 +23,6 @@ module.exports = {
     }
   },
 
-  // Load image background on bot start
   onLoad: async () => {
     const dir = path.join(__dirname, "cache", "canvas");
     const imgPath = path.join(dir, "crush.png");
@@ -40,7 +39,6 @@ module.exports = {
     }
   },
 
-  // Main run function
   onStart: async function ({ message, event }) {
     const mention = Object.keys(event.mentions);
     if (mention.length === 0)
@@ -64,14 +62,12 @@ module.exports = {
   }
 };
 
-// Helper to crop to circle
 async function circle(imagePath) {
   const img = await jimp.read(imagePath);
   img.circle();
   return await img.getBufferAsync("image/png");
 }
 
-// Main function to create couple image
 async function createCrushImage({ one, two }) {
   const dir = path.join(__dirname, "cache", "canvas");
   const bgPath = path.join(dir, "crush.png");
@@ -79,7 +75,6 @@ async function createCrushImage({ one, two }) {
   const avatar1Path = path.join(dir, `avt_${one}.png`);
   const avatar2Path = path.join(dir, `avt_${two}.png`);
 
-  // Download avatar
   const getAvatar = async (uid, savePath) => {
     const url = `https://graph.facebook.com/${uid}/picture?width=512&height=512&access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662`;
     const res = await axios.get(url, { responseType: "arraybuffer" });
@@ -89,19 +84,16 @@ async function createCrushImage({ one, two }) {
   await getAvatar(one, avatar1Path);
   await getAvatar(two, avatar2Path);
 
-  // Read images
   const bg = await jimp.read(bgPath);
   const circ1 = await jimp.read(await circle(avatar1Path));
   const circ2 = await jimp.read(await circle(avatar2Path));
 
-  // Compose final image
   bg.composite(circ1.resize(191, 191), 93, 111);
   bg.composite(circ2.resize(190, 190), 434, 107);
 
   const finalBuffer = await bg.getBufferAsync("image/png");
   fs.writeFileSync(outPath, finalBuffer);
 
-  // Clean up
   fs.unlinkSync(avatar1Path);
   fs.unlinkSync(avatar2Path);
 
