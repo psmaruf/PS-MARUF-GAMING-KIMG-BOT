5dff7104fde12501ec8c11e09586cb7904d3f03e
BAYJID-403 <merrybawm998@gmail.com>
2025-07-19 18:24:30 +0600

    Update crush.js

--- scripts/cmds/crush.js
+++ scripts/cmds/crush.js
@@ -1,58 +1,53 @@
 const fs = require("fs-extra");
 const axios = require("axios");
-const path = require("path");
 const jimp = require("jimp");
+const path = require("path");
 
 module.exports = {
   config: {
     name: "crush",
-    version: "1.0",
-    author: "Rahad",
+    version: "1.1",
+    author: "RAHAD",
     role: 0,
-    shortDescription: { en: "Show crush love photo" },
-    longDescription: { en: "Generate a crush image with your and their avatar" },
-    category: "fun",
-    guide: { en: "{pn} @mention" }
+    shortDescription: { en: "Make a crush image" },
+    longDescription: { en: "Generate crush styled image with your dp and other's" },
+    category: "image",
+    guide: { en: "{pn} @tag" }
   },
 
-  onStart: async function ({ message, event, usersData, api, args }) {
-    try {
-      const mention = Object.keys(event.mentions)[0];
-      if (!mention) return message.reply("❌ | Please mention someone to crush on 💔");
+  onStart: async function ({ message, event, api, usersData }) {
+    const mention = Object.keys(event.mentions)[0];
+    if (!mention) return message.reply("❌ | Tag your crush first!");
 
-      const one = event.senderID;
-      const two = mention;
+    const one = event.senderID;
+    const two = mention;
 
-      const userOneAvatar = `https://graph.facebook.com/${one}/picture?width=512&height=512`;
-      const userTwoAvatar = `https://graph.facebook.com/${two}/picture?width=512&height=512`;
+    const name1 = await usersData.getName(one);
+    const name2 = await usersData.getName(two);
 
-      const [avatarOne, avatarTwo] = await Promise.all([
-        jimp.read((await axios.get(userOneAvatar, { responseType: "arraybuffer" })).data),
-        jimp.read((await axios.get(userTwoAvatar, { responseType: "arraybuffer" })).data)
-      ]);
+    const avatar1 = `https://graph.facebook.com/${one}/picture?width=512&height=512`;
+    const avatar2 = `https://graph.facebook.com/${two}/picture?width=512&height=512`;
 
-      const base = await jimp.read("https://i.imgur.com/fkRAT0N.jpg"); // 🔄 Change background if needed
-      avatarOne.circle();
-      avatarTwo.circle();
+    const img1 = await jimp.read((await axios.get(avatar1, { responseType: "arraybuffer" })).data);
+    const img2 = await jimp.read((await axios.get(avatar2, { responseType: "arraybuffer" })).data);
+    const bg = await jimp.read(path.join(__dirname, "cache", "crush.png"));
 
-      base.resize(800, 600);
-      avatarOne.resize(150, 150);
-      avatarTwo.resize(150, 150);
+    img1.resize(170, 170).circle();
+    img2.resize(170, 170).circle();
 
-      base.composite(avatarOne, 170, 220);
-      base.composite(avatarTwo, 480, 220);
+    bg.composite(img1, 180, 200);
+    bg.composite(img2, 490, 200);
 
-      const tempPath = path.join(__dirname, "cache", `crush_${one}_${two}.png`);
-      await base.writeAsync(tempPath);
+    const font = await jimp.loadFont(jimp.FONT_SANS_32_WHITE);
+    bg.print(font, 170, 400, name1);
+    bg.print(font, 480, 400, name2);
 
-      message.reply({
-        body: `💘 𝐂𝐑𝐔𝐒𝐇 𝐌𝐎𝐌𝐄𝐍𝐓 💘\n💖 ${event.senderID} + ${mention} 💖`,
-        attachment: fs.createReadStream(tempPath)
-      }, () => fs.unlinkSync(tempPath));
+    const imgPath = path.join(__dirname, "cache", `crush-${one}-${two}.png`);
+    await bg.writeAsync(imgPath);
 
-    } catch (err) {
-      console.error(err);
-      return message.reply("❌ | Error creating crush image. Try again later.");
-    }
+    return message.reply({
+      body: `💘 | ${name1} + ${name2} = Crush confirmed!`,
+      attachment: fs.createReadStream(imgPath)
+    }, () => fs.unlinkSync(imgPath));
   }
 };
